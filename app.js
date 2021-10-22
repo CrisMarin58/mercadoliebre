2a0a084647b30b90c86c2fb8e3733d86219d7aea
CrisMarin58 <cgabrielamarin@gmail.com>
2021-10-21 23:56:15 -0300

    Mejoras

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
-app.use(express.static("public"));
-
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 
 app.listen(port, ()=> console.log("I´m like a bird"));
 
 
 app.get("/",(req,res)=> {
-	res.sendFile(__dirname + "/views/home.html");
+	res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/",(req,res)=> {
